a18a061a5be4a7416ca96a9e0ce1783e56c97477
Ma <159453767+luiz4a@users.noreply.github.com>
2024-10-21 12:31:56 +0000

    update

--- projeto/config/connection.py
+++ projeto/config/connection.py
@@ -12,3 +12,21 @@ db_name = "meu_banco"
 
 # A URL de conexão para BD MySQL.
 DATABASE_URL = f"mysql+pymysql://{db_user}:{db_passsword}@{db_host}:{db_port}/{db_name}"
+
+
+# Conectando ao banco de dados 
+db = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=db)
+session = Session()
+
+@contextmanager
+def get_db():
+    db = Session()              #Cria uma sessão para açoes no banco de dados.
+    try:
+        yield db                #Caso a sessão realize todas as tarefas, salva a opreação 
+        db.commit()
+    except Exception as erro:
+        db.rollback()           # Desfaz todas as alterações em caso de erro em alguma operação 
+        raise erro              # Lança um exceção
+    finally:
+        db.close()              # Fecha sessão com banco de dados. 
