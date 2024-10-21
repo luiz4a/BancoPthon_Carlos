8ff9e02ec22d1fd4538381a86da7b6c2563e8d5e
Ma <159453767+luiz4a@users.noreply.github.com>
2024-10-21 12:14:37 +0000

    update

--- projeto/config/connection.py
+++ projeto/config/connection.py
@@ -2,5 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager 
 
+
+# Parâmetros de conexão com MySQL.
+db_user = "user"
+db_passsword = "USer_password"
+db_host = "localhost"
+db_port = "3306"
+db_name = "meu_banco"
+
 # A URL de conexão para BD MySQL.
-DATABASE_URL = f"mysql+pymysql://usuario:senha@host:porta"
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_passsword}@{db_host}:{db_port}/{db_name}"
