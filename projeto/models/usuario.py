537e440a225049be4b4da4e73098a22045fc6195
Ma♡ <159453767+luiz4a@users.noreply.github.com>
2024-10-28 08:51:30 -0300

    Update usuario.py

--- projeto/models/usuario.py
+++ projeto/models/usuario.py
@@ -5,6 +5,7 @@ from config.connection import db
 Base = declarative_base()
 
 class Usuario(Base):
+    #Definindo caracteristicas da tabela no banco de dados.
     __tablename__ = "usuarios"
 
     id = Column (Integer, primary_key = True, autoincrement = True )
@@ -12,7 +13,7 @@ class Usuario(Base):
     email = Column(String(150), unique=True) #unique=True não aceita email ou dado repetido 
     senha = Column(String(150))
 
-
+    #Definindo caracteristicas da classe.
     def __init__(self, nome: str, email: str, senha: str):
         self.nome = nome 
         self.email = email 
